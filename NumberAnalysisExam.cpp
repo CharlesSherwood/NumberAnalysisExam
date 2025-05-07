ced2a90ee45f224848773ab6dabd994dbaf94536
Charles Sherwood <kulan876321@yahoo.com>
2025-05-07 10:34:11 -0400

    Add project files.

---
+++ NumberAnalysisExam.cpp
@@ -0,0 +1,18 @@
+/*FileName:NumberAnalysisExam.cpp
+Programmer Name:Charles Sherwood
+Date:5/2025
+Requirements:This Program Should Ask The User For A File 
+And Go Through The File And Display Whats In It 
+It Should Display The Lowest Number The Highest Number 
+The Total Numbers And The Average Of The Numbers.
+*/
+
+#include<iostream>
+
+int main()
+{
+
+
+
+
+}
